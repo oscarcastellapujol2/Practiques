144cb33c9b449dce64d2e2ae7fb96b2753478702
oscarcastella2019 <47317101+oscarcastella2019@users.noreply.github.com>
2020-01-27 21:59:16 +0100

    Initial commit

---
+++ Tres_Arduinos/Tres_Arduinos.ino
@@ -0,0 +1,37 @@
+
+int i;                                      // variable
+int leds [8] = {5, 6, 7, 8, 9, 10, 11, 12};
+const int marxa = A2;
+const int entrada = A0;
+const int sortida = A1;
+const int t = 500;
+
+void setup()
+{
+  for (i = 0; i < 8; i++)
+  {
+    pinMode (leds[i], OUTPUT); // definir els pins com a sortides
+  }
+  pinMode (marxa, INPUT);
+  pinMode (entrada, INPUT);
+  pinMode (sortida, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead ( marxa) == HIGH || digitalRead (entrada) == HIGH)
+
+  {
+    for (i = 0; i < 8; i++)
+    {
+      digitalWrite (i, HIGH);
+      delay (t);
+      digitalWrite (i, LOW);
+    }
+    digitalWrite(sortida, HIGH);
+    delay(t);
+    digitalWrite(sortida, LOW);
+  }
+
+
+}
